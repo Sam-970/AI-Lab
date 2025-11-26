fd3b272ae8deb486a20ef14e9fdddac92b72dabd
Your Name <you@example.com>
2025-11-26 22:20:53 +0500

    first commit

---
+++ 7_PythonModifyString.py
@@ -0,0 +1,22 @@
+# Upper case 
+x = "my name is Samia and my father name is Sajid"
+print(x.upper())
+
+#Lowercase
+x = "MY NAME IS Samia AND MY FATHER NAME IS Sajid"
+print(x.lower())
+
+# Removing Whitespace
+
+x = "   Samia, Sajid "
+print(x.strip())  
+
+# Replace string 
+
+x = "This is data  science tutorials"
+print(x.replace("This" , "Bahi"))
+
+# Split string 
+
+x = "This, is, data , science, tutorials"
+print(x.split(","))
